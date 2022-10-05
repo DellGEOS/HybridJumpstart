c88b41d8e8537f1e5550e6fd2295c691b75b826a
Matt McSpirit <mattmc@outlook.com>
2022-10-05 09:27:01 -0700

    Update LabConfig.ps1

--- dsc/LabConfig.ps1
+++ dsc/LabConfig.ps1
@@ -5,10 +5,10 @@ $LabConfig = @{ DomainAdminName = 'LabAdmin'; AdminPassword = 'LS1setup!'; DCEdi
 }
 
 # Deploy domain-joined Azure Stack HCI Nodes
-1..<<azsHostCount>> | ForEach-Object { 
+1..<<azureStackHCINodes>> | ForEach-Object { 
     $VMNames = "AzSHCI" ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; `
             ParentVHD = 'AzSHCI21H2_G2.vhdx' ; HDDNumber = 12; HDDSize = 4TB ; `
-            MemoryStartupBytes = <<azsHostMemory>>GB; MGMTNICs = 4 ; NestedVirt = $true ; VMProcessorCount = "Max"
+            MemoryStartupBytes = <<azureStackHCINodeMemory>>GB; MGMTNICs = 4 ; NestedVirt = $true ; VMProcessorCount = "Max"
     } 
 }
 
