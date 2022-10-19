9d0a84804f06758db0942ffbcf449ffca9c0f2a2
Matt McSpirit <mattmc@outlook.com>
2022-10-19 15:22:05 -0700

    Updated deployment scripts

--- dsc/Scripts/DeployHybridJumpstart.ps1
+++ dsc/Scripts/DeployHybridJumpstart.ps1
@@ -1,12 +1,8 @@
 param 
 (
-    [ValidateSet("1", "2", "3", "4")]
-    [Int]$azureStackHCINodes,
-    [ValidateSet("4", "8", "12", "16", "24", "32", "48")]
-    [Int]$azureStackHCINodeMemory,
-    [ValidateSet("Full", "Basic", "None")]
+    [String]$azureStackHCINodes,
+    [String]$azureStackHCINodeMemory,
     [String]$telemetryLevel,
-    [ValidateSet("Yes", "No")]
     [String]$updateImages,
     [String]$jumpstartPath,
     [String]$AzureStackHCIIsoPath,
@@ -15,7 +11,6 @@ param
 )
 
 $Global:VerbosePreference = "SilentlyContinue"
-$Global:ErrorActionPreference = 'Stop'
 $Global:ProgressPreference = 'SilentlyContinue'
 
 try {
@@ -23,7 +18,7 @@ try {
     # Verify Running as Admin
     $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
     If (-not $isAdmin) {
-        Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Start-Sleep -Seconds 1
+        Write-Host "-- Restarting as Administrator" -ForegroundColor Yellow ; Start-Sleep -Seconds 1
 
         if ($PSVersionTable.PSEdition -eq "Core") {
             Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
@@ -33,7 +28,6 @@ try {
         }
         exit
     }
-
     if (Get-NetConnectionProfile | Where-Object { $_.NetworkCategory -eq "Public" }) {
         Get-NetConnectionProfile | Where-Object { $_.NetworkCategory -eq "Public" } | `
             Set-NetConnectionProfile -NetworkCategory Private -ErrorAction SilentlyContinue
@@ -44,7 +38,7 @@ try {
     Write-Host "Checking PSRemoting to allow PowerShell DSC to run..."
     Enable-PSRemoting -Force -SkipNetworkProfileCheck
     Write-Host "PSRemoting enabled..."
-2
+
     # Firstly, validate if Hyper-V is installed and prompt to enable and reboot if not
     Write-Host "Checking if required Hyper-V role/features are installed..."
     $hypervState = ((Get-WindowsOptionalFeature -Online -FeatureName *Hyper-V*) | Where-Object { $_.State -eq "Disabled" })
@@ -55,7 +49,7 @@ try {
         }
         Write-Host "Do you wish to enable them now?" -ForegroundColor Green
         if ((Read-Host "(Type Y or N)") -eq "Y") {
-            Write-Host "You chose to install the required Hyper-V role/features.`nYour machine will reboot once completed.`nRerun this script when back online..."
+            Write-Host "`nYou chose to install the required Hyper-V role/features.`nYour machine will reboot once completed.`nRerun this script when back online..."
             Start-Sleep -Seconds 10
             $reboot = $false
             foreach ($feature in $hypervState) {
@@ -88,37 +82,141 @@ try {
     }
 
     if (!($azureStackHCINodes)) {
-        while ($azureStackHCINodes -notin ("1", "2", "3", "4")) {
-            $azureStackHCINodes = Read-Host "Select the number of Azure Stack HCI nodes you'd like to deploy - Enter 1, 2, 3 or 4"
+        $AskForNodeCount = {
+            $azureStackHCINodes = Read-Host "`nSelect the number of Azure Stack HCI nodes you'd like to deploy - Enter 1, 2, 3 or 4 (or Q to exit)..."
+            switch ($azureStackHCINodes) {
+                '1' { Write-Host "`nYou have chosen to deploy $azureStackHCINodes Azure Stack HCI nodes..." -ForegroundColor Green }
+                '2' { Write-Host "`nYou have chosen to deploy $azureStackHCINodes Azure Stack HCI nodes..." -ForegroundColor Green }
+                '3' { Write-Host "`nYou have chosen to deploy $azureStackHCINodes Azure Stack HCI nodes..." -ForegroundColor Green }
+                '4' { Write-Host "`nYou have chosen to deploy $azureStackHCINodes Azure Stack HCI nodes..." -ForegroundColor Green }
+                'Q' {
+                    Write-Host 'Exiting...' -ForegroundColor Red
+                    Start-Sleep -seconds 5
+                    break 
+                }
+                default {
+                    Write-Host "Invalid node count entered. Try again." -ForegroundColor Yellow
+                    .$AskForNodeCount
+                }
+            }
         }
+        .$AskForNodeCount
+        if ($azureStackHCINodes -ne 'Q') {
+            $azureStackHCINodes = [convert]::ToInt32($azureStackHCINodes)
+        }
+        else {
+            break
+        }
+    }
+    elseif ($azureStackHCINodes -notin ("1", "2", "3", "4")) {
+        Write-Host "Incorrect number of Azure Stack HCI nodes specified.`nPlease re-run the script using with either 1, 2, 3 or 4 Azure Stack HCI nodes" -ForegroundColor Red
+        break
+    }
+    elseif ($azureStackHCINodes -in ("1", "2", "3", "4")) {
+        $azureStackHCINodes = [convert]::ToInt32($azureStackHCINodes)
     }
 
     if (!($azureStackHCINodeMemory)) {
-        while ($azureStackHCINodeMemory -notin ("4", "8", "12", "16", "24", "32", "48")) {
-            $azureStackHCINodeMemory = Read-Host "Select the amount of memory in GB for each of your Azure Stack HCI nodes - Enter 4, 8, 12, 16, 24, 32, or 48"
+        $AskForNodeMemory = {
+            $azureStackHCINodeMemory = Read-Host "`nSelect the memory for each of your Azure Stack HCI nodes - Enter 4, 8, 12, 16, 24, 32, or 48 (or Q to exit)..."
+            switch ($azureStackHCINodeMemory) {
+                '4' { Write-Host "`nYou have chosen $($azureStackHCINodeMemory)GB memory for each of your Azure Stack HCI nodes..." -ForegroundColor Green }
+                '8' { Write-Host "`nYou have chosen $($azureStackHCINodeMemory)GB memory for each of your Azure Stack HCI nodes..." -ForegroundColor Green }
+                '12' { Write-Host "`nYou have chosen $($azureStackHCINodeMemory)GB memory for each of your Azure Stack HCI nodes..." -ForegroundColor Green }
+                '16' { Write-Host "`nYou have chosen $($azureStackHCINodeMemory)GB memory for each of your Azure Stack HCI nodes..." -ForegroundColor Green }
+                '24' { Write-Host "`nYou have chosen $($azureStackHCINodeMemory)GB memory for each of your Azure Stack HCI nodes..." -ForegroundColor Green }
+                '32' { Write-Host "`nYou have chosen $($azureStackHCINodeMemory)GB memory for each of your Azure Stack HCI nodes..." -ForegroundColor Green }
+                '48' { Write-Host "`nYou have chosen $($azureStackHCINodeMemory)GB memory for each of your Azure Stack HCI nodes..." -ForegroundColor Green }
+                'Q' {
+                    Write-Host 'Exiting...' -ForegroundColor Red
+                    Start-Sleep -seconds 5
+                    break 
+                }
+                default {
+                    Write-Host "Invalid memory amount entered. Try again." -ForegroundColor Yellow
+                    .$AskForNodeMemory
+                }
+            }
         }
+        .$AskForNodeMemory
+        if ($azureStackHCINodeMemory -ne 'Q') {
+            $azureStackHCINodeMemory = [convert]::ToInt32($azureStackHCINodeMemory)
+        }
+        else {
+            break
+        }
+    }
+    elseif ($azureStackHCINodeMemory -notin ("4", "8", "12", "16", "24", "32", "48")) {
+        Write-Host "Incorrect amount of memory for your Azure Stack HCI nodes specified.`nPlease re-run the script using with either 4, 8, 12, 16, 24, 32, or 48" -ForegroundColor Red
+        break
+    }
+    elseif ($azureStackHCINodeMemory -in ("4", "8", "12", "16", "24", "32", "48")) {
+        $azureStackHCINodeMemory = [convert]::ToInt32($azureStackHCINodeMemory)
     }
 
     if (!($telemetryLevel)) {
-        while ($telemetryLevel -notin ("Full", "Basic", "None")) {
-            $telemetryLevel = Read-Host "Select the telemetry level for the deployment. This helps to improve the deployment experience - Enter Full, Basic or None"
+        $AskForTelemetry = {
+            $telemetryLevel = Read-Host "`nSelect the telemetry level for the deployment. This helps to improve the deployment experience.`nEnter Full, Basic or None (or Q to exit)..."
+            switch ($telemetryLevel) {
+                'Full' { Write-Host "`nYou have chosen a telemetry level of $telemetryLevel for the deployment..." -ForegroundColor Green }
+                'Basic' { Write-Host "`nYou have chosen a telemetry level of $telemetryLevel for the deployment..." -ForegroundColor Green }
+                'None' { Write-Host "`nYou have chosen a telemetry level of $telemetryLevel for the deployment..." -ForegroundColor Green }
+                'Q' {
+                    Write-Host 'Exiting...' -ForegroundColor Red
+                    Start-Sleep -seconds 5
+                    break 
+                }
+                default {
+                    Write-Host "Invalid telemetry level entered. Try again." -ForegroundColor Yellow
+                    .$AskForTelemetry
+                }
+            }
         }
+        .$AskForTelemetry
+    }
+    elseif ($telemetryLevel -notin ("Full", "Basic", "None")) {
+        Write-Host "Invalid -telemetryLevel entered.`nPlease re-run the script with either Full, Basic or None." -ForegroundColor Red
+        break
+    }
+    elseif ($telemetryLevel -in ("Full", "Basic", "None")) {
+        Write-Host "`nYou have chosen a telemetry level of $telemetryLevel for the deployment..." -ForegroundColor Green
     }
 
     if (!($updateImages)) {
-        while ($updateImages -notin ("Y", "N")) {
-            $updateInput = Read-Host "Do you wish to update your Azure Stack HCI and Windows Server images automatically? This will increase deployment time. Enter Y or N"
+        while ($updateInput -notin ("Y", "N", "Q")) {
+            $updateInput = Read-Host "`nDo you wish to update your Azure Stack HCI and Windows Server images automatically?`nThis will increase deployment time. Enter Y or N (or Q to exit)..."
             if ($updateInput -eq "Y") {
+                Write-Host "`nYou have chosen to update your images that are created during this process.\nThis will add additional time, but your images will have the latest patches." -ForegroundColor Green
                 $updateImages = "Yes"
             }
-            else {
+            elseif ($updateInput -eq "N") {
+                Write-Host "`nYou have chosen not to update your images - you can patch VMs once they've been deployed." -ForegroundColor Yellow
                 $updateImages = "No"
+            }
+            elseif ($updateInput -eq "Q") {
+                Write-Host 'Exiting...' -ForegroundColor Red
+                Start-Sleep -Seconds 5
+                break 
+            }
+            else {
+                Write-Host "Invalid entry. Try again." -ForegroundColor Yellow
             }
         }
     }
+    elseif ($updateImages -notin ("Y", "N")) {
+        Write-Host "Invalid entry for -updateImages.`nPlease re-run the script with either Yes or No." -ForegroundColor Red
+        break
+    }
+    elseif ($updateImages -eq "Yes") {
+        Write-Host "`nYou have chosen to update your images that are created during this process.`nThis will add additional time, but your images will have the latest patches." -ForegroundColor Green
+    }
+    elseif ($updateImages -eq "No") {
+        Write-Host "`nYou have chosen not to update your images - you can patch VMs once they've been deployed." -ForegroundColor Yellow
+    }
 
     if (!($jumpstartPath)) {
-        Write-Host "Please select folder for deployment of the Hybrid Jumpstart lab infrastructure..."
+        Write-Host "`nPlease select folder for deployment of the Hybrid Jumpstart lab infrastructure..."
+        Start-Sleep -Seconds 5
         Add-Type -AssemblyName System.Windows.Forms
         $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
             RootFolder  = "MyComputer"
@@ -126,21 +224,24 @@ try {
         }
         if ($FolderBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
             $jumpstartPath = $FolderBrowser.SelectedPath
-            Write-Host "Folder selected is $jumpstartPath" -ForegroundColor Green
+            Write-Host "`nFolder selected is $jumpstartPath" -ForegroundColor Green
         }
         else {
-            Write-Host "No valid path was selected. Exiting process..." -ForegroundColor Red
+            Write-Host "No valid path was selected. Exiting..." -ForegroundColor Red
+            Start-Sleep -Seconds 5
             exit
         }
     }
 
     if (!($skipWSisoDownload)) {
         if (!($WindowsServerIsoPath)) {
-            Write-Host "Have you downloaded a Windows Server 2022 ISO? If not, one will be downloaded automatically for you"
+            Write-Host "`nHave you downloaded a Windows Server 2022 ISO? If not, one will be downloaded automatically for you"
             $wsIsoAvailable = Read-Host "Enter Y or N"
             if ($wsIsoAvailable -eq "Y") {
-                Write-Host "Please select a Windows Server 2022 ISO..."
-                [reflection.assembly]::loadwithpartialname("System.Windows.Forms")
+                Write-Host "`nPlease select a Windows Server 2022 ISO..."
+                Start-Sleep -Seconds 3
+                Add-Type -AssemblyName System.Windows.Forms
+                #[reflection.assembly]::loadwithpartialname("System.Windows.Forms")
                 $openFile = New-Object System.Windows.Forms.OpenFileDialog -Property @{
                     Title = "Please select a Windows Server 2022 ISO..."
                 }
@@ -151,22 +252,25 @@ try {
                 } 
                 if (!$openFile.FileName) {
                     Write-Host "No valid ISO file was selected... Exiting" -ForegroundColor Red
+                    Start-Sleep -Seconds 5
                     break
                 }
             }
             else {
-                Write-Host "No Windows Server 2022 ISO has been provided. One will be downloaded for you during deployment." -ForegroundColor Green
+                Write-Host "`nNo Windows Server 2022 ISO has been provided. One will be downloaded for you during deployment." -ForegroundColor Green
             }
         }
     }
 
     if (!($skipAzSHCIisoDownload)) {
         if (!($AzureStackHCIIsoPath)) {
-            Write-Host "Have you downloaded an Azure Stack HCI ISO? If not, one will be downloaded automatically for you"
+            Write-Host "`nHave you downloaded an Azure Stack HCI ISO? If not, one will be downloaded automatically for you"
             $AzSIsoAvailable = Read-Host "Enter Y or N"
             if ($AzSIsoAvailable -eq "Y") {
-                Write-Host "Please select latest Azure Stack HCI ISO..."
-                [reflection.assembly]::loadwithpartialname("System.Windows.Forms")
+                Write-Host "`nPlease select latest Azure Stack HCI ISO..."
+                Start-Sleep -Seconds 3
+                Add-Type -AssemblyName System.Windows.Forms
+                #[reflection.assembly]::loadwithpartialname("System.Windows.Forms")
                 $openFile = New-Object System.Windows.Forms.OpenFileDialog -Property @{
                     Title = "Please select latest Azure Stack HCI ISO..."
                 }
@@ -176,18 +280,21 @@ try {
                     $AzureStackHCIIsoPath = $($openfile.FileName)
                 } 
                 if (!$openFile.FileName) {
-                    Write-Host "No valid ISO file was selected... Exiting" -ForegroundColor Red
+                    Write-Host "`nNo valid ISO file was selected... Exiting" -ForegroundColor Red
+                    Start-Sleep -Seconds 5
                     break
                 }
             }
             else {
-                Write-Host "No Azure Stack HCI ISO has been provided. One will be downloaded for you during deployment." -ForegroundColor Green
+                Write-Host "`nNo Azure Stack HCI ISO has been provided. One will be downloaded for you during deployment." -ForegroundColor Green
             }
         }
     }
 
     # Download the Hybrid Jumpstart DSC files, and unzip them to C:\HybridJumpstartHost, then copy the PS modules to the main PS modules folder
-    Write-Host "Downloading the Hybrid Jumpstart lab files to C:\hybridjumpstart.zip..."
+    Write-Host "`nStarting Hybrid Jumpstart Deployment - please do not close this PowerShell window"
+    Start-Sleep -Seconds 3
+    Write-Host "`nDownloading the Hybrid Jumpstart lab files to C:\hybridjumpstart.zip..."
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest -Uri 'https://github.com/DellGEOS/HybridJumpstart/raw/main/dsc/HybridJumpstart.zip' `
         -OutFile 'C:\hybridjumpstart.zip' -UseBasicParsing -ErrorAction Stop
@@ -212,7 +319,7 @@ try {
     # Change location to where the MOFs are located, then execute the DSC configuration
     Set-Location .\HybridJumpstart\
 
-    Write-Host "Starting Hybrid Jumpstart deployment....a VMconnect ICON on your desktop will indicate completion..." -ForegroundColor Green
+    Write-Host "Starting Hybrid Jumpstart deployment....an remote desktop icon on your desktop will indicate completion..." -ForegroundColor Green
     Set-DscLocalConfigurationManager  -Path . -Force
     Start-DscConfiguration -Path . -Wait -Force -Verbose
 }
