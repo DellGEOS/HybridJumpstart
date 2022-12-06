65e2e8cbe2139ec586855079d1c63a7806eddf6b
Matt McSpirit <mattmc@outlook.com>
2022-12-06 14:01:31 -0800

    Updated LabConfig for NetATC

--- dsc/LabConfig.ps1
+++ dsc/LabConfig.ps1
@@ -2,14 +2,14 @@
 $LabConfig = @{ DomainAdminName = 'LabAdmin'; AdminPassword = 'LS1setup!'; DCEdition = '4'; ServerISOFolder = '<<WSServerIsoFolder>>'; `
                 ServerMSUsFolder = '<<MsuFolder>>'; DomainNetbiosName = 'Dell'; DefaultOUName = "HybridJumpstart"; DomainName = "dell.hybrid"; `
                 Internet = $true ; TelemetryLevel = '<<TelemetryLevel>>'; AutoStartAfterDeploy = $true; VMs = @(); AutoClosePSWindows = $true; `
-                AutoCleanUp = $true; SwitchName = "vSwitch"; Prefix = "<<VmPrefix>>-";
+                AutoCleanUp = $true; SwitchName = "vSwitch"; Prefix = "<<VmPrefix>>-"; AllowedVLANs="1-10,711-719"; AdditionalNetworksConfig=@()
 }
 
 # Deploy domain-joined Azure Stack HCI Nodes
 1..<<azureStackHCINodes>> | ForEach-Object { 
         $VMNames = "AzSHCI" ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; `
                         ParentVHD = 'AzSHCI22H2_G2.vhdx' ; HDDNumber = 12; HDDSize = 4TB ; `
-                        MemoryStartupBytes = <<azureStackHCINodeMemory>>GB; MGMTNICs = 4 ; NestedVirt = $true ; VMProcessorCount = "Max"
+                        MemoryStartupBytes = <<azureStackHCINodeMemory>>GB; MGMTNICs = 4 ; vTPM=$true ; NestedVirt = $true ; VMProcessorCount = "Max"
         } 
 }
 
