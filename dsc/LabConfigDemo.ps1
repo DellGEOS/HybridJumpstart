d59b91f38a4540022a2d2c6926798cd9cb461b25
Matt McSpirit <mattmc@outlook.com>
2023-10-30 10:55:29 -0700

    LabConfigDemo

---
+++ dsc/LabConfigDemo.ps1
@@ -0,0 +1,18 @@
+# Define core lab characteristics
+$LabConfig = @{ DomainAdminName = 'LabAdmin'; AdminPassword = 'LS1setup!'; DCEdition = '4'; ServerISOFolder = '<<WSServerIsoPath>>'; `
+                ServerMSUsFolder = '<<MsuFolder>>'; DomainNetbiosName = 'SnoValleyCoffee'; DefaultOUName = "SnoValleyCoffee"; DomainName = "snovalleycoffee.com"; `
+                Internet = $true ; TelemetryLevel = '<<TelemetryLevel>>'; AutoStartAfterDeploy = $true; VMs = @(); AutoClosePSWindows = $true; `
+                AutoCleanUp = $true; SwitchName = "vSwitch"; Prefix = "<<VmPrefix>>-"; AllowedVLANs="1-10,711-719"; `
+                CustomDnsForwarders=@("<<customDNSForwarders>>"); AdditionalNetworksConfig=@()
+}
+
+# Deploy domain-joined Azure Stack HCI Nodes
+1..<<azureStackHCINodes>> | ForEach-Object { 
+        $VMNames = "AzSHCI" ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; `
+                        ParentVHD = 'AzSHCI23H2_G2.vhdx' ; HDDNumber = 12; HDDSize = 4TB ; `
+                        MemoryStartupBytes = <<azureStackHCINodeMemory>>GB; MGMTNICs = 4 ; vTPM=$true ; NestedVirt = $true ; VMProcessorCount = "Max"; Unattend="NoDjoin"
+        } 
+}
+
+# Deploy Windows Admin Center Management Server
+$LabConfig.VMs += @{ VMName = 'WACGW' ; ParentVHD = 'Win2022Core_G2.vhdx' ; MGMTNICs = 1 }
