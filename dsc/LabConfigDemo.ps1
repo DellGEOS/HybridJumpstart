db4cfa7c252e2ff667afcf1769fc4ea44dbc337c
Matt McSpirit <mattmc@outlook.com>
2023-11-03 14:31:31 -0700

    Updated VM List

--- dsc/LabConfigDemo.ps1
+++ dsc/LabConfigDemo.ps1
@@ -8,7 +8,21 @@ $LabConfig = @{ DomainAdminName = 'LabAdmin'; AdminPassword = 'LS1setup!'; DCEdi
 
 # Deploy domain-joined Azure Stack HCI Nodes
 1..<<azureStackHCINodes>> | ForEach-Object { 
-        $VMNames = "AzSHCI" ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; `
+        $VMNames = "Duvall-N" ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; `
+                        ParentVHD = 'AzSHCI22H2_G2.vhdx' ; HDDNumber = 12; HDDSize = 4TB ; `
+                        MemoryStartupBytes = <<azureStackHCINodeMemory>>GB; MGMTNICs = 4 ; vTPM=$true ; NestedVirt = $true ; VMProcessorCount = "Max"; Unattend="NoDjoin"
+        } 
+}
+
+1..<<azureStackHCINodes>> | ForEach-Object { 
+        $VMNames = "Carnation-N" ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; `
+                        ParentVHD = 'AzSHCI22H2_G2.vhdx' ; HDDNumber = 12; HDDSize = 4TB ; `
+                        MemoryStartupBytes = <<azureStackHCINodeMemory>>GB; MGMTNICs = 4 ; vTPM=$true ; NestedVirt = $true ; VMProcessorCount = "Max"; Unattend="NoDjoin"
+        } 
+}
+
+1..<<azureStackHCINodes>> | ForEach-Object { 
+        $VMNames = "Snoqualmie-N" ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; `
                         ParentVHD = 'AzSHCI22H2_G2.vhdx' ; HDDNumber = 12; HDDSize = 4TB ; `
                         MemoryStartupBytes = <<azureStackHCINodeMemory>>GB; MGMTNICs = 4 ; vTPM=$true ; NestedVirt = $true ; VMProcessorCount = "Max"; Unattend="NoDjoin"
         } 
