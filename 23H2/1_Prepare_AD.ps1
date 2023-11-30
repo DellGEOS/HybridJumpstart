a66b46ba19acd6465acdcd988b4837455e006917
Matt McSpirit <mattmc@outlook.com>
2023-11-30 13:00:13 -0800

    Demo scripts

---
+++ 23H2/1_Prepare_AD.ps1
@@ -0,0 +1,26 @@
+### 1. Prepare Active Directory ###
+
+# Capture AD Configuration Parameters
+$AsHCIOUName = "OU=AzSClus1,DC=dell,DC=hybrid"
+$Servers = "AzSHCI1", "AzSHCI2"
+$DomainFQDN = $env:USERDNSDOMAIN
+$ClusterName = "AzSClus1"
+$Prefix = "AzSClus1"
+$UserName = "AzSClus1-DeployUser"
+$Password = "LS1setup!LS1setup!"
+$SecuredPassword = ConvertTo-SecureString $password -AsPlainText -Force
+$Credentials = New-Object System.Management.Automation.PSCredential ($UserName, $SecuredPassword)
+
+#Install required PowerShell Modules on Management Server
+Install-PackageProvider -Name NuGet -Force
+Install-Module AsHciADArtifactsPreCreationTool -Repository PSGallery -Force
+Install-Module -Name Az.Accounts -Force
+Install-Module -Name Az.Resources -Force
+
+# Install Features on Management Server
+Install-WindowsFeature -Name "RSAT-AD-PowerShell", "RSAT-ADDS", "GPMC", "RSAT-Clustering"
+
+# Configure Active Directory
+New-HciAdObjectsPreCreation -Deploy -AzureStackLCMUserCredential $Credentials `
+    -AsHciOUName $AsHCIOUName -AsHciPhysicalNodeList $Servers -DomainFQDN $DomainFQDN `
+    -AsHciClusterName $ClusterName -AsHciDeploymentPrefix $Prefix
---
+++ 23H2/2_Prepare_AzSHCI_Nodes.ps1
@@ -0,0 +1,126 @@
+### 2. Prepare AzSHCI Nodes ###
+
+# Log in to Azure from Management Server
+if (-not (Get-AzContext)) {
+    Connect-AzAccount -UseDeviceAuthentication
+}
+
+# Choose correct Azure Subscription
+$subscriptions = Get-AzSubscription
+if (($subscriptions).count -gt 1) {
+    $SubscriptionID = ($Subscriptions | Out-GridView -OutputMode Single -Title "Please Select Subscription").ID
+    $Subscriptions | Where-Object ID -eq $SubscriptionID | Select-AzSubscription
+}
+else {
+    $SubscriptionID = $subscriptions.id
+}
+
+# Configure AzSHCI Node Configuration Parameters
+$Servers = "AzSHCI1", "AzSHCI2"
+$ResourceGroupName = "AzSClus1-RG"
+$TenantID = (Get-AzContext).Tenant.ID
+$SubscriptionID = (Get-AzContext).Subscription.ID
+$Location = "eastus"
+$Cloud = "AzureCloud"
+$UserName = "Administrator"
+$Password = "LS1setup!"
+$SecuredPassword = ConvertTo-SecureString $password -AsPlainText -Force
+$Credentials = New-Object System.Management.Automation.PSCredential ($UserName, $SecuredPassword)
+
+# Configure Trusted Hosts to be able to communicate with servers (not secure)
+$TrustedHosts = @()
+$TrustedHosts += $Servers
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value $($TrustedHosts -join ',') -Force
+
+# Remotely install AzSHCI Node Features and Latest Updates
+Invoke-Command -ComputerName $servers -ScriptBlock {
+    Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -Online -NoRestart
+    Install-WindowsFeature -Name Failover-Clustering
+} -Credential $Credentials
+
+# Update Servers
+Invoke-Command -ComputerName $servers -ScriptBlock {
+    New-PSSessionConfigurationFile -RunAsVirtualAccount -Path $env:TEMP\VirtualAccount.pssc
+    Register-PSSessionConfiguration -Name 'VirtualAccount' -Path $env:TEMP\VirtualAccount.pssc -Force
+} -ErrorAction Ignore -Credential $Credentials
+
+# Inject Sleep Pause
+Start-Sleep 2
+
+# Run Windows Update via ComObject.
+Invoke-Command -ComputerName $servers -ConfigurationName 'VirtualAccount' -ScriptBlock {
+    $Searcher = New-Object -ComObject Microsoft.Update.Searcher
+    $SearchCriteriaAllUpdates = "IsInstalled=0 and DeploymentAction='Installation' or
+                                IsInstalled=0 and DeploymentAction='OptionalInstallation' or
+                                IsPresent=1 and DeploymentAction='Uninstallation' or
+                                IsInstalled=1 and DeploymentAction='Installation' and RebootRequired=1 or
+                                IsInstalled=0 and DeploymentAction='Uninstallation' and RebootRequired=1"
+    $SearchResult = $Searcher.Search($SearchCriteriaAllUpdates).Updates
+    if ($SearchResult.Count -gt 0) {
+        $Session = New-Object -ComObject Microsoft.Update.Session
+        $Downloader = $Session.CreateUpdateDownloader()
+        $Downloader.Updates = $SearchResult
+        $Downloader.Download()
+        $Installer = New-Object -ComObject Microsoft.Update.Installer
+        $Installer.Updates = $SearchResult
+        $Result = $Installer.Install()
+        $Result
+    }
+} -Credential $Credentials
+
+# Clean Up Temp PSsession
+Invoke-Command -ComputerName $servers -ScriptBlock {
+    Unregister-PSSessionConfiguration -Name 'VirtualAccount'
+    Remove-Item -Path $env:TEMP\VirtualAccount.pssc
+}  -Credential $Credentials
+
+# Reboot Nodes
+Restart-Computer -ComputerName $Servers -Credential $Credentials -WsmanAuthentication Negotiate -Wait -For PowerShell
+
+# Inject a pause to allow for full reboot
+Start-Sleep 20 
+
+# Check for AzSHCI nodes fully restarted
+Foreach ($Server in $Servers) {
+    do { $Test = Test-NetConnection -ComputerName $Server -CommonTCPPort WINRM }while ($test.TcpTestSucceeded -eq $False)
+}
+
+# Install Nuget
+Invoke-Command -ComputerName $Servers -ScriptBlock {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+} -Credential $Credentials
+
+# Install AzsHCI.ARCinstaller module on nodes
+Invoke-Command -ComputerName $Servers -ScriptBlock {
+    Install-Module -Name AzsHCI.ARCinstaller -Force
+} -Credential $Credentials
+
+# Install Az.Accounts module  on nodes
+Invoke-Command -ComputerName $Servers -ScriptBlock {
+    Install-Module -Name Az.Accounts -Force
+} -Credential $Credentials
+
+# Install Az.ConnectedMachine module on nodes
+Invoke-Command -ComputerName $Servers -ScriptBlock {
+    Install-Module -Name Az.ConnectedMachine -Force
+} -Credential $Credentials
+
+# Install Az.Resources module on nodes
+Invoke-Command -ComputerName $Servers -ScriptBlock {
+    Install-Module Az.Resources -Force
+} -Credential $Credentials
+
+# Deploy the Arc Agent and Configuration on all nodes
+$ARMtoken = (Get-AzAccessToken).Token
+$id = (Get-AzContext).Account.Id
+Invoke-Command -ComputerName $Servers -ScriptBlock {
+    Invoke-AzStackHciArcInitialization -SubscriptionID $using:SubscriptionID `
+        -ResourceGroup $using:ResourceGroupName -TenantID $using:TenantID -Cloud $using:Cloud `
+        -Region $Using:Location -ArmAccessToken $using:ARMtoken -AccountID $using:id
+} -Credential $Credentials
+
+# Ensure nodes only have a single NIC with a Default Gateway
+Invoke-Command -ComputerName $servers -ScriptBlock {
+    Get-NetIPConfiguration | Where-Object IPV4defaultGateway | Get-NetAdapter | Sort-Object Name `
+    | Select-Object -Skip 1 | Set-NetIPInterface -Dhcp Disabled
+} -Credential $Credentials
